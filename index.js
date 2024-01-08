7a24f1884856dc719a2e488bd68af7059a9b3b9e
Emelie Rosendahl <89969808+Erm0es@users.noreply.github.com>
2024-01-08 15:19:53 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const addButton = document.getElementById("add-button")
+const inputField = document.getElementById("input-field")
+
+addButton.addEventListener("click" ,function(){
+    const inputValue = inputField.value
+
+    console.log(inputValue)
+})
