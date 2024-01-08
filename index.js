1433570134e7118deb7c446540e1ef15ef5455bf
Emelie Rosendahl <89969808+Erm0es@users.noreply.github.com>
2024-01-08 16:08:23 +0100

    FireBase setup

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import {getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "*HIDDEN*"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
 const addButton = document.getElementById("add-button")
 const inputField = document.getElementById("input-field")
 
 addButton.addEventListener("click" ,function(){
-    const inputValue = inputField.value
+    let inputValue = inputField.value
+    push(shoppingListInDB, inputValue)
 
     console.log(inputValue)
 })
