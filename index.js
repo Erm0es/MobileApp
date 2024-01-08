9df6e254e614db843d78050647d2bd908aee0425
Emelie Rosendahl <89969808+Erm0es@users.noreply.github.com>
2024-01-08 17:23:14 +0100

    bug fix, db showed items 2 times

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import {getDatabase, ref, push, onValue} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "*HIDDEN*"
@@ -14,28 +14,35 @@ const inputField = document.getElementById("input-field")
 const shoppinglistEl = document.getElementById("shopping-list")
 
 
-addButton.addEventListener("click" ,function(){
+addButton.addEventListener("click", function () {
     let inputValue = inputField.value
     push(shoppingListInDB, inputValue)
 
-    appendItemToShoppingListEl(inputValue)
     clearInputFieldValue()
-   
-})
-
-onValue(shoppingListInDB, function(snapshot) {
-    let arr = Object.values(snapshot.val())
-    console.log(arr)
 
 })
 
-function clearInputFieldValue(){
-    inputField.value = ""
-    
+onValue(shoppingListInDB, function (snapshot) {
+    let itemsArr = Object.values(snapshot.val())
+
+    clearShoppingListEl()
+
+
+    for (let i = 0; i < itemsArr.length; i++) {
+        let items = itemsArr[i]
+        appendItemToShoppingListEl(items)
+    }
+})
+
+function clearShoppingListEl() {
+    shoppinglistEl.innerHTML = ""
 }
 
-function appendItemToShoppingListEl(itemValue){
+function clearInputFieldValue() {
+    inputField.value = ""
+}
+
+function appendItemToShoppingListEl(itemValue) {
     shoppinglistEl.innerHTML += `<li>${itemValue}</li>`
-    
 }
 
