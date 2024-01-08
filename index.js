38ffe5cf05fc9a9642c1209310e80ce8491f63c2
Emelie Rosendahl <89969808+Erm0es@users.noreply.github.com>
2024-01-08 17:05:19 +0100

    refactor and database edit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import {getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import {getDatabase, ref, push, onValue} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "*HIDDEN*"
@@ -11,10 +11,31 @@ const shoppingListInDB = ref(database, "shoppingList")
 
 const addButton = document.getElementById("add-button")
 const inputField = document.getElementById("input-field")
+const shoppinglistEl = document.getElementById("shopping-list")
+
 
 addButton.addEventListener("click" ,function(){
     let inputValue = inputField.value
     push(shoppingListInDB, inputValue)
 
-    console.log(inputValue)
+    appendItemToShoppingListEl(inputValue)
+    clearInputFieldValue()
+   
 })
+
+onValue(shoppingListInDB, function(snapshot) {
+    let arr = Object.values(snapshot.val())
+    console.log(arr)
+
+})
+
+function clearInputFieldValue(){
+    inputField.value = ""
+    
+}
+
+function appendItemToShoppingListEl(itemValue){
+    shoppinglistEl.innerHTML += `<li>${itemValue}</li>`
+    
+}
+
