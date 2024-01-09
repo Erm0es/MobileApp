6d4bd705d8c83c3ad149d23944bcd6ef9f4a61fd
Emelie Rosendahl <89969808+Erm0es@users.noreply.github.com>
2024-01-09 17:21:21 +0100

    Last changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebas
 import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
-    databaseURL: "*HIDDEN"
+    databaseURL: "*HIDDEN*"
 }
 
 const app = initializeApp(appSettings)
